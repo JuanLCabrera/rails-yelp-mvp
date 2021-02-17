e7ecd0b6499e200cfdbe7ec1fe9384d3b9446202
JuanCabrera <jlcabrera333@gmail.com>
2021-02-17 15:36:07 -0700

    Restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+juan_cafe = Restaurant.new({
+  name: "Juan Cafe",
+  address: "BLABLABLA 2321",
+  phone_number: "323273838",
+  category: "italian"
+  })
+
+juan_cafe.save
